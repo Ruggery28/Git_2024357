f18ec5891d9d6760489f10d417cd93954f3c5b6d
Ruggery <ruggeryalves@hotmail.com>
2024-12-09 22:02:02 +0000

    added a simple print

--- src/git_2024357/Git_2024357.java
+++ src/git_2024357/Git_2024357.java
@@ -15,6 +15,8 @@ public class Git_2024357 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.print("Ruggery Alves Marques - 2024357");
     }
     
 }
